ee7d023b3bb847c11bffc38d1df4819f2e01604c
Juan Vilchez <juanvilchez2002@gmail.com>
2023-06-04 16:23:39 -0500

    1er commmit

---
+++ src/Doctor.java
@@ -0,0 +1,11 @@
+public class Doctor {
+    //atributos
+    int id;
+    String name;
+    String speciality;
+
+    //comportamientos - la forma de susar los atributos
+    public void showName(){
+        System.out.println(name);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        Doctor myDoctor = new Doctor();
+        myDoctor.name = "Juan Vilchez";
+        myDoctor.showName();
+    }
+}
